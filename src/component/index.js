09e079d0f9a5bb5a7ae7d6e55973304b999f7380
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-21 01:28:02 +0530

    [feat] FormError and Input component

--- src/component/index.js
+++ src/component/index.js
@@ -1,2 +1,4 @@
 export { Navbar } from './navbar/Navbar';
 export { Sidebar } from './sidebar/Sidebar';
+export { Input } from './input/Input';
+export { FormError } from './form-error/FormError';
---
+++ src/component/input/Input.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './Input.css';
+export const Input = ({
+  label,
+  type,
+  placeholder,
+  autoComplete,
+  required,
+  value,
+  changeHandler,
+}) => {
+  const [obscure, setObscure] = useState(true);
+
+  return (
+    <div className='input-group'>
+      <label className={required && 'required'} htmlFor={label}>
+        {label}
+      </label>
+      <div className={`${type === 'password' && 'pass-toggle'}`}>
+        <input
+          className='input'
+          type={obscure ? type : 'text'}
+          id={label}
+          value={value}
+          placeholder={placeholder}
+          required={required}
+          autoComplete={autoComplete}
+          onChange={changeHandler}
+        />
+        {type === 'password' && (
+          <i
+            onClick={() => setObscure((prev) => !prev)}
+            className={`fas icon ${
+              obscure ? 'hide-pass fa-eye-slash' : 'show-pass fa-eye'
+            }`}
+          ></i>
+        )}
+      </div>
+    </div>
+  );
+};
