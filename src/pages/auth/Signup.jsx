3bad909d8bcca6c74889c2085b04acb989b38ee8
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-22 23:56:34 +0530

    [style] Signup form center

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -25,84 +25,82 @@ export const Signup = () => {
   };
 
   return (
-    <main className='full-container flex-row'>
-      <div className='form-container'>
-        <form className='px-2 py-2' onSubmit={submitHandler}>
-          <div className='input-row'>
-            <Input
-              label={'First Name'}
-              required={true}
-              type={'text'}
-              id={'firstname'}
-              value={userInput.firstname ?? ''}
-              autoComplete={'given-name'}
-              placeholder={'First Name'}
-              changeHandler={onInput}
-            />
-
-            <Input
-              label={'Last Name'}
-              required={true}
-              type={'text'}
-              autoComplete={'family-name'}
-              placeholder={'Last Name'}
-              id={'lastname'}
-              value={userInput.lastname ?? ''}
-              changeHandler={onInput}
-            />
-          </div>
-
-          <div className='input-row'>
-            <Input
-              type={'email'}
-              label={'Email'}
-              required={true}
-              id={'email'}
-              value={userInput.email ?? ''}
-              placeholder={'Email'}
-              changeHandler={onInput}
-            />
-          </div>
-
+    <div className='form-container flex-column flex-center'>
+      <form className='px-2 py-2' onSubmit={submitHandler}>
+        <div className='input-row'>
           <Input
-            label={'Password'}
+            label={'First Name'}
             required={true}
-            id={'password'}
-            value={userInput.password ?? ''}
-            type={'password'}
-            placeholder={'********'}
+            type={'text'}
+            id={'firstname'}
+            value={userInput.firstname ?? ''}
+            autoComplete={'given-name'}
+            placeholder={'First Name'}
             changeHandler={onInput}
           />
 
           <Input
-            label={'Confirm Password'}
+            label={'Last Name'}
             required={true}
-            type={'password'}
-            placeholder={'********'}
-            id={'confirmPassword'}
-            value={confirmPass}
-            changeHandler={(e) => setConfirmPass(e.target.value)}
+            type={'text'}
+            autoComplete={'family-name'}
+            placeholder={'Last Name'}
+            id={'lastname'}
+            value={userInput.lastname ?? ''}
+            changeHandler={onInput}
           />
+        </div>
 
-          {error && <FormError message={'Something Went Wrong'} />}
+        <div className='input-row'>
+          <Input
+            type={'email'}
+            label={'Email'}
+            required={true}
+            id={'email'}
+            value={userInput.email ?? ''}
+            placeholder={'Email'}
+            changeHandler={onInput}
+          />
+        </div>
 
-          <button
-            disabled={confirmPass !== userInput.password}
-            className='btn btn-primary btn-full my-4'
-            type='submit'
-          >
-            {loading && <i className='fas fa-circle-notch fa-spin'></i>}{' '}
-            <h4 className='fw-bold'>Create Account</h4>
-          </button>
+        <Input
+          label={'Password'}
+          required={true}
+          id={'password'}
+          value={userInput.password ?? ''}
+          type={'password'}
+          placeholder={'********'}
+          changeHandler={onInput}
+        />
 
-          <p className='fw-bold txt-center'>
-            Already have an account?
-            <Link to='/login' className='primary-link'>
-              {' Log in'}
-            </Link>
-          </p>
-        </form>
-      </div>
-    </main>
+        <Input
+          label={'Confirm Password'}
+          required={true}
+          type={'password'}
+          placeholder={'********'}
+          id={'confirmPassword'}
+          value={confirmPass}
+          changeHandler={(e) => setConfirmPass(e.target.value)}
+        />
+
+        {error && <FormError message={'Something Went Wrong'} />}
+
+        <button
+          disabled={confirmPass !== userInput.password}
+          className='btn btn-primary btn-full my-4'
+          type='submit'
+        >
+          {loading && <i className='fas fa-circle-notch fa-spin'></i>}{' '}
+          <h4 className='fw-bold'>Create Account</h4>
+        </button>
+
+        <p className='fw-bold txt-center'>
+          Already have an account?
+          <Link to='/login' className='primary-link'>
+            {' Log in'}
+          </Link>
+        </p>
+      </form>
+    </div>
   );
 };
