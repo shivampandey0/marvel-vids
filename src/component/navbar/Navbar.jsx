092f553c9a0e9d2768d5d2a0c1894c59ad497792
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-23 00:36:08 +0530

    [style][feat] Logout dropdown button

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../context';
 import './Navbar.css';
@@ -8,6 +11,12 @@ export const Navbar = () => {
     handleLogout,
     loading,
   } = useAuth();
+
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleShowDropdown = () => setShowDropdown((prev) => !prev);
+  const openDropdown = () => setShowDropdown(true);
+  const closeDropdown = () => setShowDropdown(false);
 
   return (
     <header className='header'>
@@ -30,10 +39,27 @@ export const Navbar = () => {
 
         <div>
           {token ? (
-            <button onClick={handleLogout} className='btn btn-primary'>
-              {loading && <i className='fas fa-circle-notch fa-spin'></i>}{' '}
-              {user?.firstname}
-            </button>
+            <div
+              className='btn btn-primary user'
+              onClick={toggleShowDropdown}
+              onMouseEnter={openDropdown}
+              onMouseLeave={closeDropdown}
+            >
+              <div className='flex-row align-cntr gap-1'>
+                <FaUserCircle />
+                {loading && (
+                  <i className='fas fa-circle-notch fa-spin'></i>
+                )}{' '}
+                {user?.firstname}
+              </div>
+
+              {!showDropdown ? <FiChevronDown /> : <FiChevronUp />}
+              {showDropdown ? (
+                <div className='dropdown'>
+                  <div className='dropdown-item' onClick={handleLogout}>Logout</div>
+                </div>
+              ) : null}
+            </div>
           ) : (
             <Link to={'/login'} className='btn btn-primary'>
               Login
