e81c920e06009267767164207fbfcdb46dd5b9f2
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-26 15:04:28 +0530

    [chore] added backend hosted on cyclic

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+/**
+ * backend hosted on heroku and cyclic.
+ * adding both here to switch quickly in case of down time of another
+ */
+
 export const instance = axios.create({
-  baseURL: 'https://marvel-vids.herokuapp.com',
+  baseURL: 'https://smoggy-rose-goose.cyclic.app/',
+  // baseURL: 'https://marvel-vids.herokuapp.com',
 });
