683cc85f2275077f4510f58a4664e500a220ebab
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-26 17:10:45 +0530

    [chore] removed console.log() from code

--- src/utils/server-request.js
+++ src/utils/server-request.js
@@ -4,12 +4,11 @@ import { axios } from './index';
 const getVideos = async (dispatch) => {
   try {
     const res = await axios.get(requests.videos);
-    console.log(res);
     if (res.status === 200) {
       dispatch({ type: ACTION_TYPES.VIDEOS, payload: res?.data?.videos });
     }
   } catch (error) {
-    console.log(error);
+    throw Error(error);
   }
 };
 
