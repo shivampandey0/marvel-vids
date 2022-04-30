410f1564840d5c59591775b170d77993e6b3687e
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-04-30 23:27:33 +0530

    [refactor] Modification for clear item button

--- src/component/video-card/HorizontalCard.jsx
+++ src/component/video-card/HorizontalCard.jsx
@@ -1,23 +1,32 @@
 import { useNavigate } from 'react-router-dom';
+import { MdClear } from 'react-icons/md';
 
-export const HorizontalCard = ({ video }) => {
+export const HorizontalCard = ({ video, onDeleteClick = null }) => {
   const { title, vid, creator, _id } = video;
 
   const navigate = useNavigate();
 
   return (
-    <div className='card card-h' onClick={() => navigate(`/watch/${_id}`)}>
-      <figure className='card-image'>
+    <div className='card card-h'>
+      <figure className='card-image' onClick={() => navigate(`/watch/${_id}`)}>
         <img
           width={'200'}
           src={`https://img.youtube.com/vi/${vid}/mqdefault.jpg`}
           alt={title}
         />
       </figure>
-      <div className='card-body flex-column justify-cntr'>
+      <div
+        className='card-body flex-column justify-cntr'
+        onClick={() => navigate(`/watch/${_id}`)}
+      >
         <p className='card-title h4 txt-semibold'>{title}</p>
         <small className='card-subtitle txt-grey'>{creator}</small>
       </div>
+      {onDeleteClick && (
+        <button title='Remove from History' className='order-3 btn'>
+          <MdClear className='stroke-none' onClick={onDeleteClick} />
+        </button>
+      )}
     </div>
   );
 };
