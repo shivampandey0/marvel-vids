96c3d0eae63512bf0487e6d07994c47419863ffb
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-05-03 02:19:58 +0530

    [feat] playlist page lists

--- src/pages/playlists/Playlists.jsx
+++ src/pages/playlists/Playlists.jsx
@@ -1,6 +1,33 @@
-export const Playlists = () => {
-  return (
-    <div>Playlists</div>
-  )
-}
+import { useAuth } from '../../context';
 
+export const Playlists = () => {
+  const {
+    authState: {
+      // token,
+      user: { playlists },
+    },
+    // authDispatch,
+  } = useAuth();
+
+  return (
+    <>
+      <h2 className='mx-2 my-2'>My Playlists</h2>
+      <div className='grid-4 gap-05 mx-2 my-2'>
+        {playlists &&
+          playlists.map((playlist) => (
+            <div key={playlist._id} className='card card-h'>
+              <div className='card-body'>
+                <p className='card-title h3 txt-bold my-2'>{playlist.name}</p>
+                <p className='content my-2'>{playlist.videos.length} Videos</p>
+              </div>
+              <div className='card-icons top-right'>
+                <button>
+                  <i className='fas fa-close icon'></i>
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
