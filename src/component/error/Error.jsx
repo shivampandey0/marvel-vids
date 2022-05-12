9da6f73a7951dbdacc26dd592ba162bebfba5ff9
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-05-12 19:53:09 +0530

    [feat] loader and error components

---
+++ src/component/error/Error.jsx
@@ -0,0 +1,17 @@
+export const Error = () => {
+  return (
+    <div className="flex-column flex-center container">
+      <p>Error fetching Data</p>
+      <p>Please try again Later!</p>
+    </div>
+  );
+};
+
+export const EmptyCategory = () => {
+  return (
+    <div className="flex-column flex-center container">
+      <p>No Videos Found in this category</p>
+      <p>Please try again Later!</p>
+    </div>
+  );
+};
---
+++ src/component/loader/CircleLoader.jsx
@@ -0,0 +1,7 @@
+export const CircleLoader = () => {
+  return (
+    <div className="flex-row flex-center container">
+      <i className="fas fa-circle-notch fa-spin fa-4x"></i>
+    </div>
+  );
+};
